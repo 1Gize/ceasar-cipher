7f784981a2be0d8561855d9bf43aa288cc9d6f33
1Gize <grzolko@gmail.com>
2022-04-19 10:09:44 +0200

    finished task

--- src/main/scala/CeasarCipher.scala
+++ src/main/scala/CeasarCipher.scala
@@ -1,16 +1,17 @@
 import scala.collection.mutable.ListBuffer
 
 object CeasarCipher {
-  def ceaser(word: String, offset: Int) ={
-    word.map(c => moveChar(c,offset))
+  def ceaser(word: String, offset: Int) = {
+    word.map(c => moveChar(c, offset))
 
   }
-  def moveChar(c: Char, x: Int) ={
+
+  def moveChar(c: Char, x: Int) = {
     val newChar = (c.toInt + x).toChar
-    if(newChar.toInt > 122){
+    if (newChar.toInt > 122) {
       val finalChar = (newChar.toInt - 26).toChar
       finalChar
-    }else{
+    } else {
       newChar
     }
   }
